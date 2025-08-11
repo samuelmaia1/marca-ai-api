cd588d54e9850c971e3d327cb60eb7e74c8aabb8
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2025-08-11 20:18:07 -0300

    first commit

---
+++ src/main/java/com/samuelmaia1/github/marca_ai_api/marca_ai/MarcaAiApplication.java
@@ -0,0 +1,13 @@
+package com.samuelmaia1.github.marca_ai_api.marca_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuelmaia1/github/marca_ai_api/marca_ai/MarcaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelmaia1.github.marca_ai_api.marca_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
